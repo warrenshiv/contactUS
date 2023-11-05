773ad3c44783def08408e4aceb627c3f92718cdb
warrenshiv <warrenshiv@gmail.com>
2023-11-05 04:44:35 +0300

    Included the mongodb

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,3 +1,5 @@
+import connectDB from "@/app/lib/mongodb";
+import Contact from "@/app/models/contact";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
@@ -7,7 +9,25 @@ export async function POST(req) {
     console.log("Email : ", email);
     console.log("Message: ", message);
 
-   
+   try{
+    await connectDB();
+    await Contact.create({fullname, email, message});
 
-    return NextResponse.json({ msg: ['hi from contact route.js']});
+    return NextResponse.json({
+        msg: ["Message sent successfully"], success: true
+    });
+   }catch(error){
+    if( error instanceof  mongoose.Error.ValidateError){
+        let errorList = [];
+        for (let e in error.errors){
+            errorList.push(e.message);
+        }
+
+        return NextResponse.json({msg: errorList});
+    }else {
+        return NextResponse.json({ msg: "Unable to send message."});
+    }
+   }
+
+    // return NextResponse.json({ msg: ['hi from contact route.js']});
 }
