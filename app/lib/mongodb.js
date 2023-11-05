5b617a339506cf9647ebd667202388d290b36c7c
warrenshiv <warrenshiv@gmail.com>
2023-11-05 04:43:54 +0300

    Defined the mongodb connection

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,5 +1,11 @@
+import mongoose from "mongoose";
+
 const  connectDB = async () => {
     try{
+        if(mongoose.connection.readyState === 0) {
+            await mongoose.connect(process.env.MONGODB_URL);
+            console.log("db connected");
+        }
 
     } catch(error){
         console.log(errors);
